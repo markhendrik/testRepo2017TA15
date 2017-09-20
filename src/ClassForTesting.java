1f75ac10ebf2b39f1af8f891cc8f3b489f9da086
Mark <mark.maeste@gmail.com>
2017-09-20 14:13:07 +0300

    ClassForTesting variable added.

--- src/ClassForTesting.java
+++ src/ClassForTesting.java
@@ -4,6 +4,7 @@ public class ClassForTesting {
 	public static void main(String[] args) {
 		String str = "Tere maailm!";
 		int number = -2;
+		int number2 = 9;
 		
 		System.out.println(str + number);
 		
