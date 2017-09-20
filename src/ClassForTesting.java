ac8c787c4e693452ea727afca5297111d5803038
Mark <mark.maeste@gmail.com>
2017-09-20 14:02:19 +0300

    First commit

---
+++ src/ClassForTesting.java
@@ -0,0 +1,12 @@
+
+public class ClassForTesting {
+
+	public static void main(String[] args) {
+		String str = "Tere maailm!";
+		int number = -2;
+		
+		System.out.println(str + number);
+		
+	}
+
+}
